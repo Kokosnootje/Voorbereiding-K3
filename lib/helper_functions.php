e955f160bd509003746191999f42327fbb42400c
Michiel Auerbach <michiel@auerbach.nl>
2018-12-16 08:40:16 +0100

    update removed not used fuzzy dumping of data

--- lib/helper_functions.php
+++ lib/helper_functions.php
@@ -1,70 +1,7 @@
 <?php
-//File with helper functions to make my life easier :-D
-
 /**
- * * Dumps nicely formed data on HTML page
- * @param $data
- * @return string
+ * File with helper functions to make my life easier :-D
  */
-function d($data){
-    if(is_null($data)){
-        $str = "<i>NULL</i>";
-    }elseif($data == ""){
-        $str = "<i>Empty</i>";
-    }elseif(is_array($data)){
-        if(count($data) == 0){
-            $str = "<i>Empty array.</i>";
-        }else{
-            $str = "<table style=\"border-bottom:0px solid #000;\" cellpadding=\"0\" cellspacing=\"0\">";
-            foreach ($data AS $key => $value) {
-                $str .= "<tr><td style=\"background-color:#008B8B; color:#FFF;border:1px solid #000;\">" . $key . "</td><td style=\"border:1px solid #000;\">" . d($value) . "</td></tr>";
-            }
-            $str .= "</table>";
-        }
-    }elseif(is_resource($data)){
-        while($arr = mysql_fetch_array($data)){
-            $data_array[] = $arr;
-        }
-        $str = d($data_array);
-    }elseif(is_object($data)){
-        $str = d(get_object_vars($data));
-    }elseif(is_bool($data)){
-        $str = "<i>" . ($data ? "True" : "False") . "</i>";
-    }else{
-        $str = $data;
-        $str = preg_replace("/\n/", "<br>\n", $str);
-    }
-    return $str;
-}
-
-/**
- * @param $data
- */
-function dnl($data){
-    echo d($data) . "<br>\n";
-}
-
-/**
- * @param $data
- */
-function dump($data){
-    echo dnl($data);
-}
-
-/**
- * @param $data
- */
-function dd($data){
-    echo dnl($data);
-    exit;
-}
-
-/**
- * @param string $message
- */
-function ddt($message = ""){
-    echo "[" . date("Y/m/d H:i:s") . "]" . $message . "<br>\n";
-}
 
 /**
  * Store a session message to be shown in a next page/ session load
@@ -111,3 +48,4 @@ function Back() {
 
     Redirect($url, false);
 }
+
